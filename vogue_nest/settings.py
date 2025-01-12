b540d095e7461f96428f5fcf0197ea5a6452bc11
Jawahir Abdul-Bari <714591@student.cityofbristol.ac.uk>
2025-01-12 00:33:23 +0000

    Add Toasts and re style the page

--- vogue_nest/settings.py
+++ vogue_nest/settings.py
@@ -58,12 +58,16 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+
 
 SITE_ID = 1
 
